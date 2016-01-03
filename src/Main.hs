adcbc69530c38f63c30ac1401cccef6a6b4adecf
Lingnan Dai <lingnan.d@gmail.com>
2016-01-03 22:43:43 +0000

    simple example to test the EventSelector and KeysDown Dynamic

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,39 @@
 {-# LANGUAGE LambdaCase #-}
-import Data.Colour
+import Linear
+import qualified Data.Set as S
+import Data.Default
 import Data.Colour.Names
-import Control.Monad
+import Control.Monad.IO.Class
+import Control.Lens
 import JavaScript.Cocos2d
-import JavaScript.Cocos2d.Sys
-import JavaScript.Cocos2d.EGLView
-import JavaScript.Cocos2d.Director
 import JavaScript.Cocos2d.Types
-import JavaScript.Cocos2d.Node
-import JavaScript.Cocos2d.Layer
-import JavaScript.Cocos2d.Scene
+import Reflex
+import Reflex.Cocos2d
 
 main = do
-    view <- getView
-    getSys >>= getOS >>= \case
-        IOS -> setEnableRetina view True
+    getOS >>= \case
+        IOS -> setEnableRetina True
         x -> putStrLn $ "OS: " ++ show x
-    setAdjustViewPort view True
-    setDesignResolutionSize view 960 640 ShowAll
-    setResizeWithBrowserSize view True
-    join $ runScene <$> getDirector <*> do
-        scene <- createScene
-        setOnEnter scene $ addChild scene =<< createLayerColor blueviolet
-        return scene
+    setAdjustViewPort True
+    setDesignResolutionSize 960 640 ShowAll
+    setResizeWithBrowserSize True
+    winSize <- getWinSize
+    runCocos2dHost $ do
+        evts <- globalEvents
+        dks <- dynKeysDown (select evts KeyPressed)
+                           (select evts KeyReleased)
+        sc <- scene $ do
+            layerColor (def & color .~ constDyn blueviolet) $ do
+                let unit ks = foldl (app ks) zero [ (ArrowUp, _y +~ 1)
+                                                  , (ArrowDown, _y -~ 1)
+                                                  , (ArrowLeft, _x -~ 1)
+                                                  , (ArrowRight, _x +~ 1) ]
+                    app ks v (k, op) | S.member k ks = op v
+                                     | otherwise = v
+                    start = winSize / 2
+                dpos <- forDyn dks $ (+start) . (*100) . unit
+                addVoidAction $ liftIO . print <$> updated dpos
+                layerColor_ $ def & position .~ dpos
+                                  & size .~ constDyn (pure 150)
+                                  & color .~ constDyn orange
+        liftIO $ runScene sc
