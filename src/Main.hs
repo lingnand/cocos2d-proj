678a2174d09a55cef30dcf6a3e06999abce14374
Lingnan Dai <lingnan.d@gmail.com>
2016-01-05 01:15:51 +0000

    modified for change in API

--- src/Main.hs
+++ src/Main.hs
@@ -12,28 +12,25 @@ import Reflex.Cocos2d
 
 main = do
     getOS >>= \case
-        IOS -> setEnableRetina True
+        Just IOS -> setEnableRetina True
         x -> putStrLn $ "OS: " ++ show x
     setAdjustViewPort True
     setDesignResolutionSize 960 640 ShowAll
     setResizeWithBrowserSize True
     winSize <- getWinSize
-    runCocos2dHost $ do
+    mainScene $ do
         evts <- globalEvents
         dks <- dynKeysDown (select evts KeyPressed)
                            (select evts KeyReleased)
-        sc <- scene $ do
-            layerColor (def & color .~ constDyn blueviolet) $ do
-                let unit ks = foldl (app ks) zero [ (ArrowUp, _y +~ 1)
-                                                  , (ArrowDown, _y -~ 1)
-                                                  , (ArrowLeft, _x -~ 1)
-                                                  , (ArrowRight, _x +~ 1) ]
-                    app ks v (k, op) | S.member k ks = op v
-                                     | otherwise = v
-                    start = winSize / 2
-                dpos <- forDyn dks $ (+start) . (*100) . unit
-                addVoidAction $ liftIO . print <$> updated dpos
-                layerColor_ $ def & position .~ dpos
-                                  & size .~ constDyn (pure 150)
-                                  & color .~ constDyn orange
-        liftIO $ runScene sc
+        layerColor (def & color .~ constDyn blueviolet) $ do
+            let dir ks = foldl (app ks) zero [ (ArrowUp, _y +~ 1)
+                                             , (ArrowDown, _y -~ 1)
+                                             , (ArrowLeft, _x -~ 1)
+                                             , (ArrowRight, _x +~ 1) ]
+                app ks v (k, op) | S.member k ks = op v
+                                 | otherwise = v
+            dpos <- forDyn dks $ (+winSize/2) . (*100) . dir
+            performEvent_ $ liftIO . print <$> updated dpos
+            layerColor_ $ def & position .~ dpos
+                              & size .~ constDyn (pure 150)
+                              & color .~ constDyn orange
