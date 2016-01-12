74cd6bb7178612ca376b559f62c7b51549b04c7c
Lingnan Dai <lingnan.d@gmail.com>
2016-01-12 02:37:53 +0000

    Updated to include recursive rebuild via Fix

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,45 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE LambdaCase #-}
 import Linear
-import qualified Data.Set as S
+import Data.Fix
 import Data.Default
 import Data.Colour.Names
-import Control.Monad.IO.Class
+import Control.Monad
+import Control.Compose
 import Control.Lens
-import JavaScript.Cocos2d
-import JavaScript.Cocos2d.Types
 import Reflex
 import Reflex.Cocos2d
+import JavaScript.Cocos2d
+import JavaScript.Cocos2d.Node
+import JavaScript.Cocos2d.Types
+import qualified Platformer as P
 
 main = do
-    getOS >>= \case
-        Just IOS -> setEnableRetina True
-        x -> putStrLn $ "OS: " ++ show x
+    os <- getOS
+    putStrLn $ "OS: " ++ show os
+    case os of
+      Just IOS -> setEnableRetina True
+      _ -> return ()
     setAdjustViewPort True
     setDesignResolutionSize 960 640 ShowAll
     setResizeWithBrowserSize True
     winSize <- getWinSize
     mainScene $ do
-        evts <- globalEvents
-        dks <- dynKeysDown (select evts KeyPressed)
-                           (select evts KeyReleased)
-        layerColor (def & color .~ constDyn blueviolet) $ do
-            let dir ks = foldl (app ks) zero [ (ArrowUp, _y +~ 1)
-                                             , (ArrowDown, _y -~ 1)
-                                             , (ArrowLeft, _x -~ 1)
-                                             , (ArrowRight, _x +~ 1) ]
-                app ks v (k, op) | S.member k ks = op v
-                                 | otherwise = v
-            dpos <- forDyn dks $ (+winSize/2) . (*100) . dir
-            performEvent_ $ liftIO . print <$> updated dpos
-            layerColor_ $ def & position .~ dpos
-                              & size .~ constDyn (pure 150)
-                              & color .~ constDyn orange
+      evts <- uiEvents
+      dks <- dynKeysDown (evts^.keyPressed) (evts^.keyReleased)
+      ts <- ticks
+      rec let newChild = unO . unFix <$> switchPromptlyDyn dyns
+          (_, dyns)  <- newChild & layerColor (def & color .~ constDyn blueviolet) |- do
+            [lTouched, rTouched] <- forM [(200, yellow), (400, red)] $ \(x, c) -> do
+                l <- layerColor $ def & position .~ constDyn (V2 x (winSize^._y/2))
+                                      & size .~ constDyn (pure 100)
+                                      & color .~ constDyn c
+                forHMaybe (attachDyn (l^.size) (evts^.touchesBegan)) $ \((V2 w h), locs) -> do
+                  results <- filterM ?? locs $ convertToNodeSpace l . (^.location)
+                                             >=> \(V2 x y) -> return (x <= w && y <= h)
+                  return $ guard (not $ null results) >> return ()
+            return $ leftmost [ const (P.platformer winSize dks ts) <$> lTouched ]
+      return ()
---
+++ src/Platformer.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiWayIf #-}
+module Platformer where
+
+import Linear
+import qualified Data.Set as S
+import Data.Fix
+import Data.Default
+import Data.Time.Clock
+import Data.Colour.Names
+import Control.Compose
+import Control.Lens
+import JavaScript.Cocos2d.Types
+import Reflex
+import Reflex.Cocos2d
+
+data Player = Player { _vel :: V2 Double
+                     , _pos :: V2 Double
+                     } deriving (Show)
+
+makeLenses ''Player
+
+gravity :: V2 Double
+gravity = V2 0 (-200)
+
+speed :: V2 Double
+speed = V2 300 180
+
+arrow :: S.Set Key -> V2 Double
+arrow ks = foldl app zero [ (ArrowUp, _y +~ 1)
+                          , (ArrowDown, _y -~ 1)
+                          , (ArrowLeft, _x -~ 1)
+                          , (ArrowRight, _x +~ 1)
+                          ]
+    where app v (k, app) | S.member k ks = app v
+                         | otherwise = v
+
+update :: (S.Set Key, NominalDiffTime) -> Player -> Player
+update (keys, dt) p = p & vel .~ v
+                        & pos +~ v*dt'
+                        & pos._y %~ max 0
+    where v = p^.vel & _x .~ dir^._x
+                     & (+gravity*dt')
+                     & _y %~ if | p^.pos._y == 0 -> const (dir^._y)
+                                | otherwise -> id
+          dir = arrow keys * speed
+          dt' = realToFrac dt
+
+platformer :: NodeGraph t m => V2 Double -> Dynamic t (S.Set Key) -> Event t NominalDiffTime -> Fix (m :. Event t)
+platformer winSize dks ts = Fix . O $ do
+      dplayer <- foldDyn update (Player zero (winSize/2)) $ attachDyn dks ts
+      dpos <- mapDyn (^.pos) dplayer
+      -- hfor (updated dpos) $ liftIO . print
+      layerColor_ $ def & position .~ dpos
+                        & size .~ constDyn (pure 150)
+                        & color .~ constDyn orange
+      return never
